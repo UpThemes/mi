2589a1623e9113cc0adc5809c4bcd81113cc4994
Chris Wallace <chris@walmedia.com>
2012-11-16 11:50:27 -0600

    Custom WP title.

--- functions.php
+++ functions.php
@@ -76,6 +76,38 @@ function mi_setup() {
 add_action( 'after_setup_theme', 'mi_setup' );
 
 /**
+ * Creates a nicely formatted and more specific title element text
+ * for output in head of document, based on current view.
+ *
+ * @since Mi 1.0
+ *
+ * @param string $title Default title text for current view.
+ * @param string $sep Optional separator.
+ * @return string Filtered title.
+ */
+function mi_wp_title( $title, $sep ) {
+	global $paged, $page;
+
+	if ( is_feed() )
+		return $title;
+
+	// Add the site name.
+	$title .= get_bloginfo( 'name' );
+
+	// Add the site description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		$title = "$title $sep $site_description";
+
+	// Add a page number if necessary.
+	if ( $paged >= 2 || $page >= 2 )
+		$title = "$title $sep " . sprintf( __( 'Page %s', 'mi' ), max( $paged, $page ) );
+
+	return $title;
+}
+add_filter( 'wp_title', 'mi_wp_title', 10, 2 );
+
+/**
  * Displays the header image as a background for the whole header section.
  *
  * @uses get_header_image() For displaying the header image as a background.
